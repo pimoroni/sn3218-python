cb50f769bfdb86fb8c452e8bb3483448ad105495
Phil Howard <github@gadgetoid.com>
2026-09-18 15:46:43 +0100

    Adopt tag-derived versioning and PyPI trusted publishing

--- sn3218/__init__.py
+++ sn3218/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '3.0.0'
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("sn3218")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 
 I2C_ADDRESS = 0x54
